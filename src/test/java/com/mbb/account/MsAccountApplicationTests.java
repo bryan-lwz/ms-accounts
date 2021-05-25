7273bc0aa284a3434ce20a05f978b41763157b96
Bryan Lim <blimm.dev@gmail.com>
2021-05-25 11:43:56 +0800

    initial commit

---
+++ src/test/java/com/mbb/account/MsAccountApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mbb.account;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsAccountApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
